fcc8ab3eaa6fce60e3a695adb3b8f1e9d75c2755
Daniel Perrefort <djperrefort@gmail.com>
2022-01-21 11:19:07 -0500

    Updates tests for key generation

--- tests/test_KeyGen.py
+++ tests/test_KeyGen.py
@@ -1,28 +1,26 @@
-"""Tests for the generation of secret keys"""
-
 import string
 from unittest import TestCase
 
-from keygen_manage import Command
+from keygen_manage import KeyGen
 
 
 class KeyGeneration(TestCase):
     """Tests for the generation of secret keys"""
 
     def test_error_on_non_positive_length(self) -> None:
-        """Test for a ``ValueError`` on a non-positive key"""
+        """Test for a ``ValueError`` on a non-positive key length"""
 
         with self.assertRaises(ValueError):
-            Command().gen_secret_key(length=0)
+            KeyGen().gen_secret_key(length=0)
 
         with self.assertRaises(ValueError):
-            Command().gen_secret_key(length=-1)
+            KeyGen().gen_secret_key(length=-1)
 
     def test_returned_length(self) -> None:
         """Tet the returned key length matches the ``length`` argument"""
 
         for i in range(10, 25, 50):
-            self.assertEqual(i, len(Command().gen_secret_key(i)))
+            self.assertEqual(i, len(KeyGen().gen_secret_key(i)))
 
     def test_sequential_not_equal(self) -> None:
         """Test sequential keys do not match"""
@@ -30,13 +28,13 @@ class KeyGeneration(TestCase):
         # This isn't really a true test for randomness - those are very involved
         # However, it will protect a developer who left behind a seed while debugging
         self.assertNotEqual(
-            Command().gen_secret_key(),
-            Command().gen_secret_key()
+            KeyGen().gen_secret_key(),
+            KeyGen().gen_secret_key()
         )
 
 
-class DefaultCharSet(TestCase):
-    """Tests for the character set used in key generation"""
+class DefaultCharacterSet(TestCase):
+    """Tests for the default character set used in key generation"""
 
     def assertSubsetChars(self, expected: str, actual: str) -> None:
         """Test if characters of the ``expected`` string are a subset of the ``actual`` string"""
@@ -48,14 +46,14 @@ class DefaultCharSet(TestCase):
     def test_contains_ascii_lower(self) -> None:
         """Test keys pull from lowercase letters"""
 
-        self.assertSubsetChars(string.ascii_lowercase, Command().default_char_set)
+        self.assertSubsetChars(string.ascii_lowercase, KeyGen().default_char_set)
 
     def test_contains_ascii_upper(self) -> None:
         """Test keys pull from uppercase letters"""
 
-        self.assertSubsetChars(string.ascii_uppercase, Command().default_char_set)
+        self.assertSubsetChars(string.ascii_uppercase, KeyGen().default_char_set)
 
     def test_contains_punctuation(self) -> None:
         """Test keys pull from punctuation"""
 
-        self.assertSubsetChars(string.punctuation, Command().default_char_set)
+        self.assertSubsetChars(string.punctuation, KeyGen().default_char_set)
